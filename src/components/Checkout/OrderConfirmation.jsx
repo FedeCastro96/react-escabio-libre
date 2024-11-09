575ba7dd52642a3da6644710a90af4f0134640fe
Fede Castro <fede.castro96@gmail.com>
2024-11-09 11:00:52 -0300

    order confirmation w ID

--- src/components/Checkout/OrderConfirmation.jsx
+++ src/components/Checkout/OrderConfirmation.jsx
@@ -1,11 +1,19 @@
+import { useParams } from "react-router-dom";
+
 const OrderConfirmation = () => {
+  const { orderId } = useParams();
   return (
     <div>
-      <h2>¡Orden confirmada!</h2>
+      <h1>¡Orden confirmada!</h1>
       <p>
         Tu orden fue confirmada y pronto nos pondremos en contacto contigo a
-        través de los datos solicitados.{" "}
+        través de los datos solicitados. Tu código de seguimiento es:{" "}
+        <b>{orderId}</b>
+        <br />
+        <br />{" "}
       </p>
+
+      <h3>¡Muchas gracias por tu compra!</h3>
     </div>
   );
 };
